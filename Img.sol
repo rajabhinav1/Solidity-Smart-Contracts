3117b31dd8dee49a1556ec76ecbd3b2f0c89d9cc
Abhinav Raj <abhinavshawarn@gmail.com>
2021-12-08 06:21:14 +0800

    Add files via upload

---
+++ Img.sol
@@ -0,0 +1,17 @@
+contract RandomSVG is ERC721URIStorage, VRFConsumerBase, Ownable {
+    uint256 public tokenCounter;
+
+    event CreatedRandomSVG(uint256 indexed tokenId, string tokenURI);
+    event CreatedUnfinishedRandomSVG(uint256 indexed tokenId, uint256 randomNumber);
+    event requestedRandomSVG(bytes32 indexed requestId, uint256 indexed tokenId); 
+@@ -38,6 +39,10 @@ contract RandomSVG is ERC721URIStorage, VRFConsumerBase, Ownable {
+        colors = ["red", "blue", "green", "yellow", "black", "white"];
+    }
+
+    function withdraw() public payable onlyOwner {
+        payable(owner()).transfer(address(this).balance);
+    }
+
+    function create() public returns (bytes32 requestId) {
+        requestId = requestRandomness(keyHash, fee);
+        requestIdToSender[requestId] = msg.sender;
